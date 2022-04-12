f13bbbadd201a30320f6a3a2b41b14abcba14a8e
herbertnicolas <hna@cin.ufpe.br>
2022-04-12 03:11:12 -0300

    codigo treino aula 33

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Equipe = (props) => {
+    return(
+        <div>
+            <Sobre name = {props.name} cargo = {props.cargo} idade = {props.idade}/> 
+            <Social insta = {props.insta} lkdin = {props.lkdin} fb = {props.fb}/>
+        </div>
+    );
+}
+
+const Sobre = (props) => {
+    return(
+        <div>
+            <h2>Olá sou o(a) {props.name}!</h2>
+            <h3>Nome: {props.name}</h3>
+            <h3>Cargo: {props.cargo}</h3>
+            <h3>Idade: {props.idade}</h3>
+            <hr/>
+            <h2>Redes:</h2>
+            <hr/>
+        </div>
+    );
+}
+
+const Social = (props) => {
+    return(
+        <div>
+            <h3><a href = {props.fb}> Facebook </a></h3>
+            <h3><a href = {props.lkdin}> LinkedIn </a></h3>
+            <h3><a href = {props.insta}> Instagram </a></h3>
+        </div>
+    );
+}
+
+function App(){
+    return (
+    <div>
+        <h1>Conheça nossa equipe:</h1>
+        <Equipe name="Herbert" cargo="Programador" idade="27 anos"
+        fb = "https://fb.com/herbertnicolass"
+        lkdin = "https://www.linkedin.com/in/herbert-nicolas/"
+        insta="https://instagram.com/herbertnicolas"/>
+        {/*<Sobre name="Herbert" cargo="Programador" idade="27 anos"/>  
+        <Sobre name="Lucas" cargo="Designer" idade="21 anos"/>  
+        */}
+        <h1>Olá Mundo!</h1>
     </div>
-  );
+    );
 }
 
 export default App;
+
