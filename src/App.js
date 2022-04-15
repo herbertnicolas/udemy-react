cddf43a95abda5fa0f3ac67745bfd091772f30fe
herbertnicolas <hna@cin.ufpe.br>
2022-04-15 13:23:05 -0300

    mapeamento usando a funçao map() e componente feed proprio

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
-import Membro from './components/Membro'
-class App extends Component{
+import Feed from './components/Feed';
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            feed:[
+                {id: 1, username: "Herbert", curtidas: 20, comentarios: 15},
+                {id: 2, username: "Paulo", curtidas: 17, comentarios: 23},
+                {id: 3, username: "Lucicleide", curtidas: 22, comentarios: 33},
+                {id: 4, username: "Ricardo", curtidas: 1, comentarios: 22}
+            ]
+        };
+    }
+
     render(){
         return(
             <div>
-                <Membro nome = "Visitante"/>
+                {this.state.feed.map((index) => {
+                    return(
+                        <Feed key={index.id} username={index.username}
+                        curtidas={index.curtidas} comentarios={index.comentarios}/>
+                    );
+                })}
             </div>
-        )
+        );
     }
 }
 
